1a0bb5c8124aa8517e99da75e645e89007f50879
MaiHuyenmt <huyenmt.ptit@gmail.com>
2025-10-07 16:59:49 +0700

    Bai tap Quan ly Thanh toan don hang

---
+++ Baitap/Quanlythanhtoan/src/ThanhToanMoMo.java
@@ -0,0 +1,6 @@
+class ThanhToanMomo implements ThanhToan {
+    public void thanhToan(String tenKhach, double tongTien) {
+        System.out.println("Khách hàng: " + tenKhach + ". Tổng tiền: " + tongTien +
+                ". Thanh toán bằng Momo thành công.");
+    }
+}
